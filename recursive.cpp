63b9f907466df41147ef4cfe6db00ae56aac5709
Griffin Speidel <32367669+ZedLeppin@users.noreply.github.com>
2020-12-07 19:13:29 -0500

    maze algorithm should be correct

--- recursive.cpp
+++ recursive.cpp
@@ -1,5 +1,6 @@
 #include <random>
 #include <string>
+#include <vector>
 #include "recursive.h"
 
 Recursive::Recursive(){
@@ -18,32 +19,54 @@ void Recursive::generateMaze(int rows, int columns){
     int starty = cy;
     int lasty = cy;
     while(true){ // loops for the whole maze
-        while(true){ // loops for the current cell
-            int dir = rand() % 4;
-            if(cy > 1){ // if not at top of board
-                if(dir == 0 && m_maze[cy - 2][cx] == WALL_CHAR){ // if looking up and the cell there is untouched
-                    cy -= 2;
-                    m_maze[cy - 1][cx] = EMPTY_CHAR;
+        int dir = rand() % 4;
+        bool adjacentEmpty = true;
+        if(cy > 1){ // if not at top of board
+            if(dir == 0 && m_maze[cy - 2][cx] == WALL_CHAR){ // if looking up and the cell there is untouched
+                m_maze[cy - 1][cx] = EMPTY_CHAR;
+                m_maze[cy - 2][cx] = EMPTY_CHAR;
+                if(cx < columns - 2 && cy < rows - 2 && cx > 1){ // makes sure it won't step out of bounds when checking all adjacent cells
+                    adjacentEmpty = !(m_maze[cy][cx + 2] == WALL_CHAR && m_maze[cy + 2][cx] == WALL_CHAR && m_maze[cy][cx - 2] == WALL_CHAR);
                 }
-            }else if(cx < columns - 2){ // if not at right of board
-                if(dir == 1 && m_maze[cy][cx + 2] == WALL_CHAR){ // if looking right and the cell there is untouched
-                    cx += 2;
-                    m_maze[cy][cx + 1] = EMPTY_CHAR;
-                }
-            }else if(cy < rows - 2){ // if not at bottom of board
-                if(dir == 2 && m_maze[cy + 2][cx] == WALL_CHAR){ // if looking down and the cell there is untouched
-                    cy += 2;
-                    m_maze[cy + 1][cx] = EMPTY_CHAR;
-                }
-            }else if(cx > 1){ // if not at left of board
-                if(dir == 3 && m_maze[cy][cx - 2] == WALL_CHAR){ // if looking left and the cell there is untouched
-                    cx -= 2;
-                    m_maze[cy][cx - 1] = EMPTY_CHAR;
-                }
-            }else{
-                cx = lastx;
-                cy = lasty;
+                cy -= 2;
             }
+        }else if(cx < columns - 2){ // if not at right of board
+            if(dir == 1 && m_maze[cy][cx + 2] == WALL_CHAR){ // if looking right and the cell there is untouched
+                m_maze[cy][cx + 1] = EMPTY_CHAR;
+                m_maze[cy][cx + 2] = EMPTY_CHAR;
+                if(cy > 1 && cy < rows - 2 && cx > 1){ // makes sure it won't step out of bounds when checking all adjacent cells
+                    adjacentEmpty = !(m_maze[cy - 2][cx] == WALL_CHAR && m_maze[cy + 2][cx] == WALL_CHAR && m_maze[cy][cx - 2] == WALL_CHAR);
+                }
+                cx += 2;
+            }
+        }else if(cy < rows - 2){ // if not at bottom of board
+            if(dir == 2 && m_maze[cy + 2][cx] == WALL_CHAR){ // if looking down and the cell there is untouched
+                m_maze[cy + 1][cx] = EMPTY_CHAR;
+                m_maze[cy + 2][cx] = EMPTY_CHAR;
+                if(cy > 1 && cx < columns - 2 && cx > 1){ // makes sure it won't step out of bounds when checking all adjacent cells
+                    adjacentEmpty = !(m_maze[cy - 2][cx] == WALL_CHAR && m_maze[cy][cx + 2] == WALL_CHAR && m_maze[cy][cx - 2] == WALL_CHAR);
+                }
+                cy += 2;
+            }
+        }else if(cx > 1){ // if not at left of board
+            if(dir == 3 && m_maze[cy][cx - 2] == WALL_CHAR){ // if looking left and the cell there is untouched
+                m_maze[cy][cx - 1] = EMPTY_CHAR;
+                m_maze[cy][cx - 2] = EMPTY_CHAR;
+                if(cy > 1 && cx < columns - 2 && cy < rows - 2){ // makes sure it won't step out of bounds when checking all adjacent cells
+                    adjacentEmpty = !(m_maze[cy - 2][cx] == WALL_CHAR && m_maze[cy][cx + 2] == WALL_CHAR && m_maze[cy + 2][cx] == WALL_CHAR);
+                }
+                cx -= 2;
+            }
+        }else{
+            cx = lastx;
+            cy = lasty;
+        }
+        if(adjacentEmpty){
+            lastx = cx;
+            lasty = cy;
+        }
+        if(cx == startx && cy == starty){
+            break;
         }
     }
 }
--- recursive.h
+++ recursive.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <vector>
 #include "mazegenerator.h"
 
 class Recursive : public MazeGenerator{
