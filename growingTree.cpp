db0a73cd6a4cd33b20040166c2177d7eaf7d8e0d
NegNebulous <ajbewley1@gmail.com>
2020-12-07 19:41:56 -0500

    added generating a random neighbor and checking if it was already visited

--- growingTree.cpp
+++ growingTree.cpp
@@ -1,6 +1,5 @@
 #include <vector>
 #include <string>
-#include <vector>
 #include "growingTree.h"
 
 namespace lineThings {
@@ -44,4 +43,54 @@ void GrowingTree::generateMaze(int rows, int columns) {
     }
 
     std::vector<lineThings::point> points;
+
+    start:
+    bool mazeIncomplete = true;
+    int curX = rand() % columns;
+    int curY = rand() % rows;
+    int lastX;
+    int lastY;
+    while (mazeIncomplete) {
+        int lastX = curX;
+        int lastY = curY;
+        while (true) {
+            std::vector<lineThings::point> pointsTemp;
+            randomNeighbor:
+            //random neighbor
+            int tempX = 0;
+            while (tempX == 0) {
+                tempX = lastX + (rand() % 4) - 1;
+            }
+            int tempY = 0;
+            while (tempY == 0) {
+                tempY = lastY + (rand() % 4) - 1;
+            }
+
+            //is neighbor visited
+            for (int i = 0; i < points.size(); i++) {
+                if ((points[i].x == tempX) && (points[i].y == tempY)) {
+
+                    for (int j = 0; j < pointsTemp.size(); j++) {
+                        if ((pointsTemp[j].x != tempX) && (pointsTemp[j].y != tempY)) {
+                            pointsTemp.emplace_back(lineThings::point(tempX, tempY));
+                        }
+                    }
+                    if (pointsTemp.size() >= 4) {
+                        //this means there are no un-visted neighbors
+                        goto start;
+                    }
+                    goto randomNeighbor;
+                }
+            }
+            
+            //
+        }
+        points.emplace_back(lineThings::point(curX, curY));
+    }
+
+    /*for (int i = 0; i < columns; i++) {
+        for (int j = 0; j < rows; j++) {
+
+        }
+    }*/
 }
