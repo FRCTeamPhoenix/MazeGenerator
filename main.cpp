51705a82c07406b0789417e607efe67835c7288b
Leo <leo.bunkartas@asdnh.org>
2020-12-10 19:34:27 -0500

    Updated main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 
 #include "mazeprogram.h"
+#include "huntandkill.h"
 
 int main(){
     MazeProgram maze;
+    HuntAndKill huntandkill;
+    maze.addGenerator("huntandkill", &huntandkill);
     maze.run();
 }
